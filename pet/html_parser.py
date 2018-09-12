f632311272943424be6eb113fd50a6db3d0c22f2
mangues <mangues@yeah.net>
2018-09-12 12:59:56 +0800

    增加文章标签

--- pet/html_parser.py
+++ pet/html_parser.py
@@ -23,15 +23,26 @@ class HtmlParser(object):
         return new_urls
 
     def _get_new_data(self, url, soup):
-        matchObj = re.match(compile_news,url)
-        if matchObj is None:
+        crumbs_node = soup.find("ul", class_="crumbs-bar")
+        ac_content_node = soup.find("div", class_="article-content")
+        if crumbs_node==None or ac_content_node==None:
             return
-
         res_data = {}
-        title_node = soup.find("h1", class_="ac-title")
+        tag_bar = []
+        find_all = crumbs_node.find_all("li")
+        for tag in find_all:
+            find = tag.find("a")
+            if find !=None:
+                text = find.get_text()
+                if text!="宠物网":
+                    tag_bar.append(text)
+
+
+        title_node = ac_content_node.find("h1", class_="ac-title")
         res_data["title"] =title_node.get_text()
-        content_node = soup.find("div", class_="ac-content").find("table")
+        content_node = ac_content_node.find("div", class_="ac-content").find("table")
         res_data["summary"] = content_node.get_text()
         res_data["content"] = str(content_node)
         res_data["url"] = url
+        res_data["tags"] = tag_bar
         return res_data;
